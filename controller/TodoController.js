2ec837cd6cf63cfc4308212d833e19481f22599b
RaiJ1n <129413551+RaiJ1n@users.noreply.github.com>
2024-07-17 21:54:04 +0800

    first commit

---
+++ controller/TodoController.js
@@ -0,0 +1,122 @@
+const Todo = require ('../model/TodoModel')
+const User = require('../model/UserModel')
+
+exports.createTask = async (req, res) => {
+    console.log(req.user.email)
+    const {todo, email} = req.body
+
+    try {
+        const getCurrentUser = await User.findOne ({email: req.user.email})
+        console.log(getCurrentUser)
+
+        const newTask = new Todo({
+            todo: todo,
+        })
+        await newTask.save()
+        getCurrentUser.taskList.push(newTask)
+        await getCurrentUser.save()
+        res.status(200).json({
+            content: newTask,
+            message: "Tasked created successfully"
+        })
+    } catch (err) {
+        res.status(403).json({
+            status: "Failed",
+            content: err.message
+        });
+    }
+}
+
+exports.getTask = async (req, res) => {
+    console.log(req.user.email);
+    try {
+      const getAllTask = await User.findOne({ email: req.user.email })
+        .populate("taskList")
+        .exec();
+  
+      if (getAllTask.taskList.length == 0) {
+        return res.sendStatus(204);
+      }
+      res.status(200).json({
+        message:"This is your tasks",
+        content: getAllTask.taskList,
+      });
+    } catch (err) {
+      res.status(400).json({
+        content: err,
+      });
+    }
+};
+
+exports.updateTask = async (req, res) => {
+    const todoid = req.params.id;
+    const { todo } = req.body;
+
+    try {
+        const updateTask = await Todo.findByIdAndUpdate(
+            todoid,
+            { todo: todo },
+            {
+                runValidators: true,
+                new: true,
+            }
+        );
+
+        console.log(updateTask);
+
+        if (updateTask) {
+            return res.status(200).json({
+                status: "Successfully Changed",
+                content: updateTask,
+            });
+        }
+
+        return res.status(204).json({
+            message: "Task not found",
+        });
+    } catch (err) {
+        return res.status(404).json({
+            message: "Internal server error", err,
+        });
+    }
+};
+
+
+exports.deleteTask = async (req, res) => {
+    const todoid = req.params.id;
+
+    try {
+        const getCurrentUser = await User.findOne({ email: req.user.email });
+
+        if (!getCurrentUser) {
+            return res.status(204).json({
+                message: "User not found",
+            });
+        }
+
+        getCurrentUser.taskList = getCurrentUser.taskList.filter(
+            (taskId) => taskId.toString() !== todoid
+        );
+        
+        
+
+
+        const deletedTask = await Todo.findByIdAndDelete(todoid);
+
+        if (!deletedTask) {
+            return res.status(203).json({
+                message: "Task not found",
+            });
+        }
+
+        res.status(200).json({
+            status: "Successfully Deleted",
+            content: deletedTask,
+        });
+    } catch (err) {
+        return res.status(404).json({
+            message: "Internal server error",
+            content: err.message,
+        });
+    }
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require ('mongoose');
+const app = express();
+const port = 4001;
+const cookieParser = require ('cookie-parser');
+const cors = require ('cors')
+const morgan = require('morgan')
+const todoRoutes = require('./routes/TodoRoutes')
+const userRoutes = require('./routes/UserRoutes')
+
+
+
+app.use(morgan('dev'))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static("public"));
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+)
+
+app.use("/todo", todoRoutes)
+app.use("/todo", userRoutes)
+
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/tododb")
+    .then(() => console.log("Connected to Database"))
+    .catch((err) => console.error("Error connecting to database"));
+
+app.listen(port, function() {
+    console.log("Server running in port " + port);
+})

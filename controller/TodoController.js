a5fb0bac4fe5a7178b6bfc06c5708e0044817343
RaiJ1n <129413551+RaiJ1n@users.noreply.github.com>
2024-08-10 22:35:47 +0800

    new

--- controller/TodoController.js
+++ controller/TodoController.js
@@ -93,8 +93,7 @@ exports.updateTask = async (req, res) => {
             });
         }
         
-        // todo.isfinished = !todo.isfinished; 
-        // await todo.save(); 
+        
 
         res.status(200).json({
             status: "Checked",
